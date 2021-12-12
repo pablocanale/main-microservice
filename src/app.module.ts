b5d2620cbac02116ea80984d7bab929345de66f9
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-12 19:16:39 -0300

    Fix- import missing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose'
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
