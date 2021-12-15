fac61f99b5a0ade44de4ca571e32584a5d78bec1
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-14 21:25:07 -0300

    Changes on dependecys

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { appendFile } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
